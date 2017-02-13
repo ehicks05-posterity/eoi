fda4bdda84578290d1ed5dfa89e6f0f523248b3e
Eric Hicks <ehicks05@yahoo.com>
2017-02-13 05:22:47 +0000

    Added support for hot-backups with MSSQL.

--- src/main/java/net/ehicks/eoi/EOIBackup.java
+++ src/main/java/net/ehicks/eoi/EOIBackup.java
@@ -2,8 +2,23 @@ package net.ehicks.eoi;
 
 public class EOIBackup
 {
+    public static String getBackupExtension()
+    {
+        if (EOI.databaseBrand.equals("h2"))
+            return ".sql";
+        if (EOI.databaseBrand.equals("sqlserver"))
+            return ".bak";
+        return ".dmp";
+    }
+
     public static void backup(String backupPath)
     {
-        EOI.executeQuery("script to '" + backupPath + "'");
+        if (EOI.databaseBrand.equals("h2"))
+            EOI.executeQuery("script to '" + backupPath + "'");
+        if (EOI.databaseBrand.equals("sqlserver"))
+        {
+            String dbName = "master";
+            EOI.execute("BACKUP DATABASE " + dbName + " TO DISK = '" + backupPath + "' WITH FORMAT;");
+        }
     }
 }
